84c2c03fda81c27c474f6f5b0de1cc2a755d04c3
NibbleAndByte <AndHalcomb@GMail.com>
2019-07-13 16:47:06 -0500

    First push.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      resultEvents: [{"stuff":"thing"}],
+    };
+  }
+  updateResults(result)
+  {
+    // debugger
+    // this.setState({resultEvents: result})
+    this.setState({resultEvents: 1})
+  }
+
+  render() {
+
+    return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and meow to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="tileContainer" className="container">
+          <Tile diceValue='8' diceQuantity='5' updateResults = {this.updateResults} />
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+          <Tile diceValue='8' diceQuantity='5'/>
+        </div>
+        <Results events = {this.state.resultEvents}/>   
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
+
+class Results extends React.Component {
+  render(){
+    return(
+      <div>
+          HELLO FROM RESULTS COMPONENT {this.props.events[0].stuff}
+      </div>
+    );
+  }
+}
+
+class Tile extends React.Component {
+  constructor(props)
+  {
+      super(props);
+      this.state = {
+        diceValue: this.props.diceValue,
+        diceQuantity: this.props.diceQuantity,
+        lastRoll: 0
+      };
+  }
+
+  roll = () => {
+    let val = 0
+    for (let i = 0; i < this.state.diceQuantity; i++)
+    {
+      val += Math.floor(Math.random() * this.state.diceValue ) + 1;
+    }
+    this.props.updateResults({value:val});
+    this.setState({lastRoll : val})
+  }
+
+  render() {
+      return(
+          <div class = "tile" onClick = {this.roll}>
+            <p>Sleep - roll 5d8.</p>
+            {
+              this.state.lastRoll > 0 && 
+              <p>Sleep: rolled {this.state.lastRoll}</p>
+            }
+          </div>
+      )
+  }
+}
